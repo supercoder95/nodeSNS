d7c78bd5bec83c498203edcedb3acc99b5e9d7b1
supercoder95 <supercoder@kakao.com>
2020-09-27 04:30:06 +0900

    react-redux is good

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "44e6d481dc2ef60fa033";
+/******/ 	var hotCurrentHash = "053df1f5ea68b3182177";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
