6b4c9d5f6aa6915240b96fbb40e52ca44e7ffc7b
supercoder <kazehazaya@gmail.com>
2020-10-08 18:20:12 +0900

    jkhhgjk

--- front/components/PostForm.js
+++ front/components/PostForm.js
@@ -1,6 +1,13 @@
-import { Form, Input } from 'antd'
+import { Form, Input, Button } from 'antd'
+import { useCallback } from 'react';
+import { useSelector } from 'react-redux';
 
 const PostForm = () => {
+    const { imagePath } = useSelector((state) => state.post)
+    const onSubmit = useCallback(() => {
+
+    }, [])
+
     return (
         <Form style={{ margin: '10px 0 20px' }} encType="multipart/form-data" onFinish={onsubmit}>
             <Input.TextArea
@@ -9,7 +16,18 @@ const PostForm = () => {
                 maxLength={140}
                 placeholder="어떤 신기한 일이 있었나요?"
             />
-            <div></div>
+            <div>
+                <input type="file" multiple hidden />
+                <Button>이미지 업로드</Button>
+                <Button type="primary" stylep={{ float: 'right' }} htmlType="submit">짹짹</Button>
+            </div>
+            <div>
+                {imagePath.map((v) => (
+                    <div key={v} style={{ display: 'inline-block' }}>
+
+                    </div>
+                ))}
+            </div>
         </Form >
 
     )
