81e75a1d27305e39078cf313acbb6b3ab9743f7e
supercoder95 <supercoder@kakao.com>
2020-10-10 01:34:32 +0900

    PostCard edit

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,10 +1,59 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { useSelector } from 'react-redux'
+import { Card, Popover, Button } from 'antd'
+import { RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined } from '@ant-design/icons'
+import Avatar from 'antd/lib/avatar/avatar'
+import PostImages from './PostImages'
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
+    const id = useSelector((state) => state.user.me?.id)
     return (
-        <div>PostCard</div>
+        <div>
+            <Card
+                cover={post.Image[0] && <PostImages images={post.Images} />}
+                actions={[
+                    <RetweetOutlined key="retweet" />,
+                    <HeartOutlined key="heart" />,
+                    <MessageOutlined key="commnet" />,
+                    <Popover key="more" content={(
+                        <Button.Group>
+                            {id && post.User.id === id
+                                ? (
+                                    <>
+                                        <Button>수정</Button>
+                                        <Button type="danger">삭제</Button>
+                                    </>
+                                )
+                                : <Button>신고</Button>}
+                        </Button.Group>
+                    )}>
+                        <EllipsisOutlined />
+                    </Popover>,
+                ]}
+            >
+                <Card.Meta
+                    avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+                    title={post.User.nickname}
+                    description={post.content}
+                />
+            </Card>
+            {/* <CommnetForm />
+            <Comments /> */}
+        </div >
     )
+}
 
-};
+PostCard.propTypes = {
+    post: PropTypes.shape({
+        id: PropTypes.number,
+        User: PropTypes.object,
+        content: PropTypes.string,
+        createAt: PropTypes.object,
+        Commnet: PropTypes.arrayOf(PropTypes.object),
+        Images: PropTypes.arrayOf(PropTypes.object),
+
+    }).isRequired,
+}
 
 export default PostCard
