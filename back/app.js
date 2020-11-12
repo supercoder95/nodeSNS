21102a79a4bfeb4f857b264504fd120dcfa0ef4c
supercoder <kazehazaya@gmail.com>
2020-11-12 18:26:45 +0900

    add

--- back/app.js
+++ back/app.js
@@ -1,8 +1,45 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    res.end('Hello node');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('hello express');
 });
-server.listen(3065, () => {
+
+app.get('/api', (req,res) => {
+    res.send('hello api');
+});
+
+app.get('/api/posts', (req,res) => {
+    res.json([
+        {
+            id: 1,
+            content: 'hello'
+        },
+          {
+            id: 2,
+            content: 'hello1'
+        },
+          {
+            id: 3,
+            content: 'hello2'
+        },
+    ])
+});
+
+app.post('/api/post', (req,res) => {
+    res.json({
+            id: 1,
+            content: 'hello'
+        });
+});
+
+app.delete('/api/post', (req,res) => {
+    res.json({
+        id: 1,
+    })
+});
+
+app.listen(3065, () => {
     console.log('서버 실행 중');
 });
